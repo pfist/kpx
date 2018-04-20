fce93eeebfd3df70462da043b8260dd3e12bc52d
Nick Pfisterer <nick@polymoon.net>
2018-04-20 14:54:57 -0700

    Add command: Disable Command

---
+++ commands/system/disable.js
@@ -0,0 +1,34 @@
+import { Command } from 'discord-akairo'
+
+class DisableCommand extends Command {
+  constructor () {
+    super('disable', {
+      aliases: ['disable'],
+      category: 'System',
+      userPermissions: ['BAN_MEMBERS'],
+      args: [
+        {
+          id: 'command',
+          type: 'command',
+          prompt: {
+            start: 'Which command would you like me to disable?',
+            retry: message => `${message.content} is not a valid command. Please try again.`
+          }
+        }
+      ]
+    })
+  }
+
+  async exec (message, args) {
+    const command = await this.handler.modules.get(args.command.id)
+
+    if (!command.enabled) {
+      return message.util.send(`The \`${args.command}\` command is already disabled.`)
+    } else {
+      await command.disable()
+      return message.util.send(`The \`${args.command}\` command has been disabled.`)
+    }
+  }
+}
+
+export default DisableCommand
