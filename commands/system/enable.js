54d4758b6b14fbe547eea594309541e4d295ef8b
Nick Pfisterer <nick@polymoon.net>
2018-04-20 14:54:49 -0700

    Add command: Enable Command

---
+++ commands/system/enable.js
@@ -0,0 +1,34 @@
+import { Command } from 'discord-akairo'
+
+class EnableCommand extends Command {
+  constructor () {
+    super('enable', {
+      aliases: ['enable'],
+      category: 'System',
+      userPermissions: ['BAN_MEMBERS'],
+      args: [
+        {
+          id: 'command',
+          type: 'command',
+          prompt: {
+            start: 'Which command would you like me to enable?',
+            retry: message => `${message.content} is not a valid command. Please try again.`
+          }
+        }
+      ]
+    })
+  }
+
+  async exec (message, args) {
+    const command = await this.handler.modules.get(args.command.id)
+
+    if (command.enabled) {
+      return message.util.send(`The \`${args.command}\` command is already enabled.`)
+    } else {
+      await command.enable()
+      return message.util.send(`The \`${args.command}\` command has been enabled.`)
+    }
+  }
+}
+
+export default EnableCommand
