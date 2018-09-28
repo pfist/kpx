91568286cf0895db9cee1df91e9ca6926a0ca9d2
Nick Pfisterer <nick@polymoon.net>
2018-09-27 21:07:43 -0700

    Add messageUpdate listener

---
+++ listeners/client/messageUpdate.js
@@ -0,0 +1,29 @@
+import { Listener } from 'discord-akairo'
+import config from '../../bot.config.js'
+
+class MessageUpdateListener extends Listener {
+  constructor () {
+    super('messageUpdate', {
+      emitter: 'client',
+      eventName: 'messageUpdate'
+    })
+  }
+
+  async exec (oldMessage, newMessage) {
+    // Message edited
+    if (oldMessage.content !== newMessage.content) {
+      const channel = await this.client.channels.find(channel => channel.name === config.messageLogChannel)
+
+      const embed = this.client.util.embed()
+      .setTitle(':pencil: MESSAGE EDITED')
+      .setDescription(`<@${oldMessage.author.id}> edited a message in ${oldMessage.channel}.`)
+      .addField('Old Message', oldMessage.content)
+      .addField('New Message', newMessage.content)
+      .setFooter(`Message ID: ${newMessage.id}`)
+
+      return channel.send(embed)
+    }
+  }
+}
+
+export default MessageUpdateListener
