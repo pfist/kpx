445f0c386ce2dda1e70a6690f49b35fcc6e6d605
Nick Pfisterer <nick@polymoon.net>
2018-09-27 21:07:51 -0700

    Add messageDelete listener

---
+++ listeners/client/messageDelete.js
@@ -0,0 +1,25 @@
+import { Listener } from 'discord-akairo'
+import config from '../../bot.config.js'
+
+class MessageDeleteListener extends Listener {
+  constructor () {
+    super('messageDelete', {
+      emitter: 'client',
+      eventName: 'messageDelete'
+    })
+  }
+
+  async exec (message) {
+    const channel = await this.client.channels.find(channel => channel.name === config.messageLogChannel)
+
+    const embed = this.client.util.embed()
+    .setTitle(':x: MESSAGED DELETED')
+    .setDescription(`<@${message.author.id}> deleted a message in ${message.channel}.`)
+    .addField('Deleted Message', message.content)
+    .setFooter(`Message ID: ${message.id}`)
+
+    return channel.send(embed)
+  }
+}
+
+export default MessageDeleteListener
