d40665ce535e6c80011bd2889c7e8316c0bba7e8
Nick Pfisterer <nick@polymoon.net>
2018-12-19 16:41:32 -0800

    Improved formatting and minor tweaks

--- commands/system/disable.js
+++ commands/system/disable.js
@@ -10,15 +10,16 @@ class DisableCommand extends Command {
         usage: '!disable <command>'
       },
       channelRestriction: 'guild',
-      userPermissions: ['BAN_MEMBERS'],
+      userPermissions: ['MANAGE_GUILD'],
+      protected: true,
       args: [
         {
           id: 'command',
-          type: 'command',
+          type: 'commandAlias',
           description: 'The command to disable',
           prompt: {
-            start: 'Which command would you like me to disable?',
-            retry: message => `${message.content} is not a valid command. Please try again.`
+            start: 'Which command do you want to disable?',
+            retry: `That is not a valid command. Please try again.`
           }
         }
       ]
@@ -28,11 +29,15 @@ class DisableCommand extends Command {
   async exec (message, args) {
     const command = await this.handler.modules.get(args.command.id)
 
+    if (command.protected) {
+      return message.util.send(`:warning: That command is **protected** and can't be disabled.`)
+    }
+
     if (!command.enabled) {
-      return message.util.send(`The \`${args.command}\` command is already disabled.`)
+      return message.util.send(`:information_source: The \`${args.command}\` command is already disabled.`)
     } else {
       await command.disable()
-      return message.util.send(`The \`${args.command}\` command has been disabled.`)
+      return message.util.send(`:no_entry_sign: The \`${args.command}\` command is now **disabled.**`)
     }
   }
 }
