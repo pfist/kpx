2e981c8a4289d9bd17ef51f86292deb4c71fc973
Nick Pfisterer <nick@polymoon.net>
2018-12-28 22:55:18 -0800

    Update uptime.js

--- commands/system/uptime.js
+++ commands/system/uptime.js
@@ -7,7 +7,7 @@ class UptimeCommand extends Command {
       aliases: ['uptime'],
       category: 'System',
       description: {
-        content: 'Get the amount of time since the bot last logged in.',
+        content: 'Get the elapsed time since the bot last logged in.',
         usage: '!uptime'
       },
       channelRestriction: 'guild',
@@ -18,7 +18,7 @@ class UptimeCommand extends Command {
 
   async exec (message) {
     const uptime = await ms(this.client.uptime, { verbose: true, secDecimalDigits: 0 })
-    return message.util.send(`:stopwatch: I have been online for ${uptime}.`)
+    return message.util.send(`:stopwatch: I have been online for **${uptime}**.`)
   }
 }
 
