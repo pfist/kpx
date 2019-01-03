417d80d001f1f9a2a3fda33373b2deb91f949c03
Nick Pfisterer <nick@polymoon.net>
2019-01-02 16:48:06 -0800

    Improved Avatar command

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -6,16 +6,16 @@ class AvatarCommand extends Command {
       aliases: ['avatar'],
       category: 'Utility',
       description: {
-        content: `Get the large version of a user's avatar, or yourself if no argument is given.`,
+        content: `See the large version of a user's avatar.`,
         usage: '!avatar [user]'
       },
       channelRestriction: 'guild',
-      userPermissions: ['VIEW_CHANNEL'],
+      userPermissions: ['SEND_MESSAGES'],
       args: [
         {
           id: 'user',
           type: 'user',
-          description: `The user who's avatar you want to get. Can be a username or ID.`,
+          description: `The user who's avatar you want to see. Can be a username or ID.`,
           default: message => message.author,
           prompt: {
             start: `Who's avatar do you want to see?`,
@@ -32,22 +32,24 @@ class AvatarCommand extends Command {
 
     // Checking the author
     if (user.id === message.author.id) {
-      return message.reply(`Here is your avatar:`, {
+      return message.reply(`Here's your avatar:`, {
         files: [avatar]
       })
     }
 
     // Checking the bot
     if (user.id === this.client.user.id) {
-      return message.util.send(`Here is my avatar:`, {
+      return message.util.send(`Here's my avatar:`, {
         files: [avatar]
       })
-    } else {
-      // Checking anyone else
-      return message.util.send(`Here is the avatar for **${user.username}**:`, {
-        files: [avatar]
-      })
+
+      return message.util.send({ embed })
     }
+
+    // Checking anyone else
+    return message.util.send(`Here's the avatar for **${user.tag}**:`, {
+      files: [avatar]
+    })
   }
 }
 
